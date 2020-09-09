9bb7e4749bc03834843b417e0655a7988ad4e790
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2020-09-09 11:23:15 +0530

    updated

--- code.cpp
+++ code.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 using namespace std;
 
-#define SQUARE(x) x*x
-
 int main() {
 
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 
-	int x = 36 / SQUARE(6);
-	cout << x;
 
 	return 0;
 }
