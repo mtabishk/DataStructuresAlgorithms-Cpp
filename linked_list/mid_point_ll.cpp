98f00128384451affe1f63145cb3e7d18683f6c4
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2020-12-04 19:04:18 +0530

    updated

---
+++ linked_list/mid_point_ll.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+Node *midPoint(Node *head) {
+	// Write your code here
+	if (head == NULL || head->next == NULL) {
+		return head;
+	}
+	Node *slow = head;
+	Node *fast = head->next;
+
+	while ( fast != NULL  ) {
+		if ( fast->next == NULL ) {
+			return slow;
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return slow;
+
+}
+
+int main()
+{
+
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		Node *mid = midPoint(head);
+		if (mid != NULL)
+		{
+			cout << mid->data;
+		}
+		cout << endl;
+	}
+	return 0;
+}
