db21b94f0b818bfd04015dc00f8c48f159854c1f
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2020-12-11 18:41:52 +0530

    tree input output

--- trees/takeInput_print_level_wise.cpp
+++ trees/takeInput_print_level_wise.cpp
@@ -18,10 +18,9 @@ public:
     }
 };
 
-#include "solution.h"
-
 TreeNode<int>* takeInputLevelWise() {
     int rootData;
+    cout << "Enter root data: " << endl;
     cin >> rootData;
     TreeNode<int>* root = new TreeNode<int>(rootData);
 
@@ -31,10 +30,12 @@ TreeNode<int>* takeInputLevelWise() {
     while (pendingNodes.size() != 0) {
         TreeNode<int>* front = pendingNodes.front();
         pendingNodes.pop();
+        cout << "Enter the number of children of " << front->data << endl;
         int numChild;
         cin >> numChild;
         for (int i = 0; i < numChild; i++) {
             int childData;
+            cout << "Enter " << i << "th child of " << front->data << endl;
             cin >> childData;
             TreeNode<int>* child = new TreeNode<int>(childData);
             front->children.push_back(child);
@@ -43,6 +44,28 @@ TreeNode<int>* takeInputLevelWise() {
     }
 
     return root;
+}
+
+void printLevelWise(TreeNode<int>* root) {
+    // Write your code here
+    queue<TreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << front->data << ":";
+
+        for (int i = 0; i < front->children.size(); i++) {
+            if (i < ((front->children.size()) - 1 ))
+                cout << front->children[i]->data << ",";
+            else
+                cout << front->children[i]->data;
+
+            pendingNodes.push(front->children[i]);
+        }
+        cout << endl;
+    }
 }
 
 int main() {
