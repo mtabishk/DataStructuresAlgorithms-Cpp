cfa41fe84c9a0ecd80b1806ec8b93e50c44d7cbe
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2020-12-11 18:19:54 +0530

    added Tree Use.cpp

---
+++ trees/TreeNodeUse.cpp
@@ -0,0 +1,31 @@
+#include "TreeNode.h"
+#include <iostream>
+using namespace std;
+
+void printTree(TreeNode<int>* root) {
+	if (root == NULL) return;
+	cout << root->data << " : ";
+
+	for (int i = 0; i < root->children.size(); i++) cout << root->children[i]->data << ",";
+
+	cout << endl;
+
+	for (int i = 0; i < root->children.size(); i++) printTree(root->children[i]);
+
+}
+int main() {
+
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+
+	TreeNode <int>* root = new TreeNode<int>(1);
+	TreeNode <int>* node1 = new TreeNode<int>(2);
+	TreeNode <int>* node2 = new TreeNode<int>(3);
+
+	root->children.push_back(node1);
+	root->children.push_back(node2);
+
+	printTree(root);
+
+	return 0;
+}
