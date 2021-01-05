2961025c59ba8630ac6b3bc07c954463626075e1
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2021-01-05 12:22:57 +0530

    take input in trees

--- binary_trees/BinaryTreeNode.h
+++ binary_trees/BinaryTreeNode.h
@@ -1,3 +1,4 @@
+#include <iostream>
 template <typename T>
 class BinaryTreeNode {
 public:
--- binary_trees/takeInput.cpp
+++ binary_trees/takeInput.cpp
@@ -2,11 +2,50 @@
 #include <iostream>
 using namespace std;
 
+void printTreeBetter(BinaryTreeNode<int> *root) {
+	if (root == NULL) return;
+
+	cout << root->data << ": ";
+
+	if (root->left != NULL) {
+		cout << "\tL: " << root->left->data;
+	}
+
+	if (root->right != NULL) {
+		cout << "\tR: " << root->right->data;
+	}
+	cout << endl;
+	printTreeBetter(root->left);
+	printTreeBetter(root->right);
+}
+
+BinaryTreeNode<int>* takeInput() {
+	int rootData;
+	cout << "Enter Data: " << endl;
+	cin >> rootData;
+
+	if (rootData == -1) {
+		return NULL;
+	}
+
+	BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+
+	BinaryTreeNode<int> *leftChild = takeInput();
+	BinaryTreeNode<int> *rightChild = takeInput();
+
+	root->left = leftChild;
+	root->right = rightChild;
+
+	return root;
+}
+
 int main() {
 
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
 
+	BinaryTreeNode<int> *root = takeInput();
+	printTreeBetter(root);
 
 	return 0;
 }
