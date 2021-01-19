2115f290c7d2b64301af9dd73563326c2833c8ee
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2021-01-19 11:25:15 +0530

    diameter of tree

---
+++ binary_trees/Diameter_Tree.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include "BinaryTreeNode.h"
+#include <queue>
+using namespace std;
+
+BinaryTreeNode<int>* takeInputLevelWise() {
+	int rootData;
+	cout << "Enter root data" << endl;
+	cin >> rootData;
+	if (rootData == -1) {
+		return NULL;
+	}
+
+	BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+
+	queue<BinaryTreeNode<int>*> pendingNodes;
+	pendingNodes.push(root);
+	while (pendingNodes.size() != 0) {
+		BinaryTreeNode<int>* front = pendingNodes.front();
+		pendingNodes.pop();
+		cout << "Enter left child of " << front->data << endl;
+		int leftChildData;
+		cin >> leftChildData;
+		if (leftChildData != -1) {
+			BinaryTreeNode<int>* child = new BinaryTreeNode<int>(leftChildData);
+			front->left = child;
+			pendingNodes.push(child);
+		}
+		cout << "Enter right child of " << front->data << endl;
+		int rightChildData;
+		cin >> rightChildData;
+		if (rightChildData != -1) {
+			BinaryTreeNode<int>* child = new BinaryTreeNode<int>(rightChildData);
+			front->right = child;
+			pendingNodes.push(child);
+		}
+	}
+	return root;
+}
+
+void printTree(BinaryTreeNode<int>* root) {
+	if (root == NULL) {
+		return;
+	}
+	cout << root->data << ":";
+	if (root->left != NULL) {
+		cout << "L" << root->left->data;
+	}
+
+	if (root->right != NULL) {
+		cout << "R" << root->right->data;
+	}
+	cout << endl;
+	printTree(root->left);
+	printTree(root->right);
+}
+
+
+
+int height(BinaryTreeNode<int>* root) {
+	if (root == NULL) {
+		return 0;
+	}
+	return 1 + max(height(root->left), height(root->right));
+}
+
+int diameter(BinaryTreeNode<int>* root) {
+	if (root == NULL) {
+		return 0;
+	}
+
+	int option1 = height(root->left) + height(root->right);
+	int option2 = diameter(root->left);
+	int option3 = diameter(root->right);
+
+	return max(option1, max(option2, option3));
+}
+
+int main() {
+	BinaryTreeNode<int>* root = takeInputLevelWise();
+	printTree(root);
+
+	int d = diameter(root);
+	cout << "Diameter: " << d << endl;
+	delete root;
+}
