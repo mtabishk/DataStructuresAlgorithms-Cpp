1edde134691d37683774d16b5a790e84828e4f90
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2021-01-27 09:54:54 +0530

    update

--- trees/contains_x_tree.cpp
+++ trees/contains_x_tree.cpp
@@ -3,46 +3,6 @@
 #include <vector>
 using namespace std;
 
-template <typename T>
-class TreeNode {
-public:
-    T data;
-    vector<TreeNode<T>*> children;
-
-    TreeNode(T data) { this->data = data; }
-
-    ~TreeNode() {
-        for (int i = 0; i < children.size(); i++) {
-            delete children[i];
-        }
-    }
-};
-
-
-TreeNode<int>* takeInputLevelWise() {
-    int rootData;
-    cin >> rootData;
-    TreeNode<int>* root = new TreeNode<int>(rootData);
-
-    queue<TreeNode<int>*> pendingNodes;
-
-    pendingNodes.push(root);
-    while (pendingNodes.size() != 0) {
-        TreeNode<int>* front = pendingNodes.front();
-        pendingNodes.pop();
-        int numChild;
-        cin >> numChild;
-        for (int i = 0; i < numChild; i++) {
-            int childData;
-            cin >> childData;
-            TreeNode<int>* child = new TreeNode<int>(childData);
-            front->children.push_back(child);
-            pendingNodes.push(child);
-        }
-    }
-
-    return root;
-}
 
 bool isPresent(TreeNode<int>* root, int x) {
     // Write your code here
@@ -53,11 +13,3 @@ bool isPresent(TreeNode<int>* root, int x) {
         if (ans == true) return true;
     }
     return false;
-
-}
-int main() {
-    TreeNode<int>* root = takeInputLevelWise();
-    int x;
-    cin >> x;
-    cout << (isPresent(root, x) ? "true" : "false");
-}
